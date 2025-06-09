43e870792a39fd0c621b070412d43f4abbe43c23
Testuser12457 <kingammar1234567810@gmail.com>
2025-06-10 00:56:53 +0300

    Create central.py

---
+++ central.py
@@ -0,0 +1,4 @@
+REPOSITORIES = { "repo_1": {"owner": "github_user1", "repo": "agents_repo_1"}, "repo_2": {"owner": "github_user2", "repo": "agents_repo_2"}, "repo_3": {"owner": "github_user3", "repo": "agents_repo_3"}, "repo_4": {"owner": "github_user4", "repo": "agents_repo_4"}, "repo_5": {"owner": "github_user5", "repo": "agents_repo_5"}, "repo_6": {"owner": "github_user6", "repo": "agents_repo_6"}, }
+TOTAL_AGENTS_RANGE = (100, 500)  # عدد الوكلاء العشوائي الكلي 
+AGENT_DELAY_RANGE = (5, 12)  # تأخير بين كل Agent والآخر
+AGENT_CONFIG_FILENAME = "agent_config.json"
