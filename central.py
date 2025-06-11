d14b53147364c598e2dc03045ecbe98dfb4f9101
Testuser12457 <kingammar1234567810@gmail.com>
2025-06-12 01:03:02 +0300

    Update central.py

--- central.py
+++ central.py
@@ -1,4 +1,4 @@
-REPOSITORIES = { "repo_1": {"owner": "Bono123445678", "repo": "subfile"}, "repo_2": {"owner": "github_user2", "repo": "agents_repo_2"}, "repo_3": {"owner": "github_user3", "repo": "agents_repo_3"}, "repo_4": {"owner": "github_user4", "repo": "agents_repo_4"}, "repo_5": {"owner": "github_user5", "repo": "agents_repo_5"}, "repo_6": {"owner": "github_user6", "repo": "agents_repo_6"}, }
+REPOSITORIES = { "repo_1": {"owner": "Bono123445678", "repo": "subfile"}, "repo_2": {"owner": "teto12356" , "repo": "subfile2"}, "repo_3": {"owner": "Beno12345678", "repo": "subfile3"}, "repo_4": {"owner": "toto1234788", "repo": "subfile4"}, "repo_5": {"owner": "wero6483628", "repo": "subfile5"}, }
 TOTAL_AGENTS_RANGE = (100, 500)  # عدد الوكلاء العشوائي الكلي 
 AGENT_DELAY_RANGE = (5, 12)  # تأخير بين كل Agent والآخر
 AGENT_CONFIG_FILENAME = "agent_config.json"
