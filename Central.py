c3492606972b8ab03fb77185147d9494680acfa9
Testuser12457 <kingammar1234567810@gmail.com>
2025-06-12 03:55:34 +0300

    Update Central.py

--- Central.py
+++ Central.py
@@ -1,4 +1,4 @@
-REPOSITORIES = { "repo_1": {"owner": "Bono12344567
+REPOSITORIES = { "repo_1": {"owner": "Bono123445678" 
 8", "repo": "subfile"}, "repo_2": {"owner": "teto12356" , "repo": "subfile2"}, "repo_3": {"owner": "Beno12345678", "repo": "subfile3"}, "repo_4": {"owner": "toto1234788", "repo": "subfile4"}, "repo_5": {"owner": "wero6483628", "repo": "subfile5"}, }
 TOTAL_AGENTS_RANGE = (100, 500)  # عدد الوكلاء العشوائي الكلي 
 AGENT_DELAY_RANGE = (5, 12)  # تأخير بين كل Agent والآخر
